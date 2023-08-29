ffbd8315de3f48ed9d38390d92be674f7bdaa738
pankx <a3427173515@163.com>
2023-08-29 22:29:12 +0800

    add : 添加统一结果返回类

---
+++ blog-security/src/main/java/com/kai/pojo/R.java
@@ -0,0 +1,61 @@
+package com.kai.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 统一结果返回
+ * @author pankx
+ * @date 2023/8/29 22:09
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class R<T> {
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 提示信息
+     */
+    private String message;
+
+    /**
+     * 返回数据
+     */
+    private T data;
+
+    public static <T> R<T> success(Integer code, String message, T data){
+        return new R<>(code, message, data);
+    }
+
+    public static <T> R<T> success(String message, T data){
+        return success(200, message, data);
+    }
+
+    public static <T> R<T> success(T data){
+        return success(null, data);
+    }
+
+    public static <T> R<T> error(Integer code, String message, T data){
+        return new R<>(code, message, data);
+    }
+
+    public static <T> R<T> error(String message, T data){
+        return error(500, message, data);
+    }
+
+    public static <T> R<T> error(String message){
+        return error(message, null);
+    }
+
+    public static <T> R<T> error(){
+        return error("服务器异常");
+    }
+}
