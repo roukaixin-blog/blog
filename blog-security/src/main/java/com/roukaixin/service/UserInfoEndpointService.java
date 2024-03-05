185abfae3b4a0e4362a2dbea91259bf3a45d7beb
pankx <a3427173515@163.com>
2024-03-05 23:13:12 +0800

    refactor: 添加 user_info_endpoint 表相关类和xml文件

---
+++ blog-security/src/main/java/com/roukaixin/service/UserInfoEndpointService.java
@@ -0,0 +1,16 @@
+package com.roukaixin.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roukaixin.pojo.UserInfoEndpoint;
+
+/**
+ * 针对表【user_info_endpoint(用户信息端点)】的数据库操作Service
+ *
+ * @author 不北咪
+ * @description 针对表【user_info_endpoint(用户信息端点)】的数据库操作Service
+ * @createDate 2024-03-05 23:04:34
+ */
+public interface UserInfoEndpointService extends IService<UserInfoEndpoint> {
+
+}
