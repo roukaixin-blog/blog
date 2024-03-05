c759b08c513b271583268d7ac7edc5f5d281e4b1
pankx <a3427173515@163.com>
2024-03-05 22:53:43 +0800

    feat: 生成provider_details数据表对应的类和xml文件

---
+++ blog-security/src/main/java/com/roukaixin/service/impl/ProviderDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.roukaixin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.roukaixin.mapper.ProviderDetailsMapper;
+import com.roukaixin.pojo.ProviderDetails;
+import com.roukaixin.service.ProviderDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 针对表【provider_details(oauth2 服务商提供的信息)】的数据库操作Service实现
+ *
+ * @author 不北咪
+ * @description 针对表【provider_details(oauth2 服务商提供的信息)】的数据库操作Service实现
+ * @createDate 2024-03-05 22:46:39
+ */
+@Service
+public class ProviderDetailsServiceImpl extends ServiceImpl<ProviderDetailsMapper, ProviderDetails>
+    implements ProviderDetailsService {
+
+}
+
+
+
+
