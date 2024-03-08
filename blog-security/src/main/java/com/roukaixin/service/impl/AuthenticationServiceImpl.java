6ae54ccd15545fbad555d68c816b53d401995da5
pankx <a3427173515@163.com>
2024-03-08 18:10:21 +0800

    feat: oauth2 回调处理

--- blog-security/src/main/java/com/roukaixin/service/impl/AuthenticationServiceImpl.java
+++ blog-security/src/main/java/com/roukaixin/service/impl/AuthenticationServiceImpl.java
@@ -7,14 +7,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationExchange;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationResponse;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 认证管理器
@@ -34,6 +50,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void oauth2RequestRedirect(String registrationId, String redirect,
                                       HttpServletRequest request, HttpServletResponse response) {
@@ -50,14 +69,96 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public void loginOauth2Code(String registrationId, HttpServletRequest request, HttpServletResponse response) {
-        // 参数中有 code 和 state 或 state 和 error 都是授权响应
-
+        // 获取请求参数
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        MultiValueMap<String, String> params = toMultiMap(parameterMap);
+        // 判断是否为认证响应,参数中有 code 和 state 或 state 和 error 都是授权响应
+        if (!isAuthorizationResponse(params)) {
+            // 不是认证响应
+            throw new RuntimeException(new OAuth2Error(OAuth2ErrorCodes.INVALID_REQUEST).toString());
+        }
         // 获取 OAuth2AuthorizationRequest，从 redis 中获取
         com.roukaixin.authorization.endpoint.OAuth2AuthorizationRequest authorizationRequest =
                 (com.roukaixin.authorization.endpoint.OAuth2AuthorizationRequest) redisTemplate.opsForValue()
                         .get(request.getSession().getId());
+        if (authorizationRequest == null) {
+            OAuth2Error oauth2Error = new OAuth2Error("authorization_request_not_found");
+            throw new RuntimeException(oauth2Error.toString());
+        }
+        OAuth2AuthorizationRequest.Builder builder = OAuth2AuthorizationRequest.authorizationCode()
+                .authorizationUri(authorizationRequest.getAuthorizationUri())
+                .clientId(authorizationRequest.getClientId())
+                .redirectUri(authorizationRequest.getRedirectUri())
+                .scopes(authorizationRequest.getScopes())
+                .state(authorizationRequest.getState())
+                .additionalParameters(authorizationRequest.getAdditionalParameters())
+                .authorizationRequestUri(authorizationRequest.getAuthorizationRequestUri())
+                .attributes(authorizationRequest.getAttributes());
+        ClientRegistration clientRegistration = jdbcClientRegistrationRepository.findByRegistrationId(registrationId);
+        String redirectUri = UriComponentsBuilder.fromHttpUrl(UrlUtils.buildFullRequestUrl(request))
+                .replaceQuery(null)
+                .build()
+                .toUriString();
+        OAuth2AuthorizationResponse authorizationResponse = convert(params, redirectUri);
+        // 调用认证管理器
+        OAuth2LoginAuthenticationToken authenticationRequest = new OAuth2LoginAuthenticationToken(clientRegistration,
+                new OAuth2AuthorizationExchange(builder.build(), authorizationResponse));
+        OAuth2LoginAuthenticationToken authenticationResult = (OAuth2LoginAuthenticationToken) authenticationManager
+                .authenticate(authenticationRequest);
+        OAuth2AuthorizedClient authorizedClient = getAuthorizedClient(authenticationResult);
+        log.info("认证信息:{}", authorizedClient);
+    }
 
+    private static OAuth2AuthorizedClient getAuthorizedClient(OAuth2LoginAuthenticationToken authenticationResult) {
+        OAuth2AuthenticationToken oauth2Authentication = new OAuth2AuthenticationToken(
+                authenticationResult.getPrincipal(), authenticationResult.getAuthorities(),
+                authenticationResult.getClientRegistration().getRegistrationId());
+        return new OAuth2AuthorizedClient(
+                authenticationResult.getClientRegistration(), oauth2Authentication.getName(),
+                authenticationResult.getAccessToken(), authenticationResult.getRefreshToken());
+    }
 
+    private OAuth2AuthorizationResponse convert(MultiValueMap<String, String> request, String redirectUri) {
+        String code = request.getFirst(OAuth2ParameterNames.CODE);
+        String errorCode = request.getFirst(OAuth2ParameterNames.ERROR);
+        String state = request.getFirst(OAuth2ParameterNames.STATE);
+        if (StringUtils.hasText(code)) {
+            return OAuth2AuthorizationResponse.success(code).redirectUri(redirectUri).state(state).build();
+        }
+        String errorDescription = request.getFirst(OAuth2ParameterNames.ERROR_DESCRIPTION);
+        String errorUri = request.getFirst(OAuth2ParameterNames.ERROR_URI);
+        // @formatter:off
+        return OAuth2AuthorizationResponse.error(errorCode)
+                .redirectUri(redirectUri)
+                .errorDescription(errorDescription)
+                .errorUri(errorUri)
+                .state(state)
+                .build();
+        // @formatter:on
+    }
+
+    private boolean isAuthorizationResponse(MultiValueMap<String, String> params) {
+        return isAuthorizationResponseSuccess(params) || isAuthorizationResponseError(params);
+    }
+
+    private boolean isAuthorizationResponseSuccess(MultiValueMap<String, String> params) {
+        return StringUtils.hasText(params.getFirst(OAuth2ParameterNames.CODE))
+                && StringUtils.hasText(params.getFirst(OAuth2ParameterNames.STATE));
+    }
+
+    private boolean isAuthorizationResponseError(MultiValueMap<String, String> params) {
+        return StringUtils.hasText(params.getFirst(OAuth2ParameterNames.ERROR))
+                && StringUtils.hasText(params.getFirst(OAuth2ParameterNames.STATE));
+    }
+
+    private MultiValueMap<String, String> toMultiMap(Map<String, String[]> parameterMap) {
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        parameterMap.forEach((k, v) -> {
+            for (String value : v) {
+                params.add(k, value);
+            }
+        });
+        return params;
     }
 
     private void sendRedirectForAuthorization(HttpServletRequest request, HttpServletResponse response,
@@ -76,8 +177,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                           HttpServletResponse response) {
         Assert.notNull(request, "request cannot be null");
         Assert.notNull(response, "response cannot be null");
-        String state = authorizationRequest.getState();
-        Assert.hasText(state, "authorizationRequest.state cannot be empty");
         // 保存 OAuth2AuthorizationRequest，回调之后会使用到 `OAuth2AuthorizationRequest`
         com.roukaixin.authorization.endpoint.OAuth2AuthorizationRequest build =
                 com.roukaixin.authorization.endpoint.OAuth2AuthorizationRequest
@@ -93,7 +192,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                         .authorizationRequestUri(authorizationRequest.getAuthorizationRequestUri())
                         .attributes(authorizationRequest.getAttributes())
                         .build();
-        redisTemplate.opsForValue().set(request.getSession().getId(), build);
+        // 保存到 redis 中，key：state
+        redisTemplate.opsForValue().set(authorizationRequest.getState(), build);
     }
 
 }
