7e72e17516074186c67605e59155cd665d68a91c
pankx <a3427173515@163.com>
2024-03-10 20:55:27 +0800

    fix: 修改异常日志没有打印到控制台

--- common/src/main/java/com/roukaixin/handler/GlobalExceptionHandler.java
+++ common/src/main/java/com/roukaixin/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.roukaixin.handler;
 
 import com.roukaixin.exception.JsonException;
 import com.roukaixin.pojo.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,21 +12,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @date 2023/8/30 10:13
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(JsonException.class)
     public R<String> jsonException(JsonException jsonException){
+        log.error("", jsonException);
         return R.error(jsonException.getMessage());
     }
 
 
     @ExceptionHandler(Exception.class)
     public R<String> exception(Exception e){
+        log.error("", e);
         return R.error(e.getMessage());
     }
 
     @ExceptionHandler(Throwable.class)
-    public R<String> throwable(){
+    public R<String> throwable(Throwable e){
+        log.error("", e);
         return R.error();
     }
 
