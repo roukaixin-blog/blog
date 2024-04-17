34c021669d12574400ea762a2dad5f3c54df565e
pankx <a3427173515@163.com>
2024-04-17 12:41:55 +0800

    chore: 类的包名错误

--- blog-security/src/main/java/com/roukaixin/security/service/impl/UserServiceImpl.java
+++ blog-security/src/main/java/com/roukaixin/security/service/impl/UserServiceImpl.java
@@ -2,9 +2,9 @@ package com.roukaixin.security.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.roukaixin.security.mapper.UserMapper;
-import com.roukaixin.service.UserService;
-import org.springframework.stereotype.Service;
 import com.roukaixin.security.pojo.User;
+import com.roukaixin.security.service.UserService;
+import org.springframework.stereotype.Service;
 
 /**
  * 针对表【sys_user(用户表)】的数据库操作Service实现
